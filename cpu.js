48338403a0aac9bb1f3062b82315098b40af5991
Leon <leon_geertinger@hotmail.com>
2026-02-23 17:35:38 +0100

    Add step() method, timers and V, I properties to cpu

--- cpu.js
+++ cpu.js
@@ -3,9 +3,29 @@ export class Cpu {
     this.memory = memory;
     this.display = display;
 
+    this.V = new Uint8Array(16);
+    this.I = 0;
     this.stack = new Uint16Array(16);
     this.stackPointer = 0;
     this.programCounter = 0x200;
+
+    this.delayTimer = 0;
+    this.soundTimer = 0;
+  }
+
+  step() {
+    //fetch 2 bytes and merge them into one 16bit instruction
+    const high = this.memory[this.programCounter];
+    const low = this.memory[this.programCounter + 1];
+    /*
+    add 8 bits of zero padding to the right of high. Then compare
+    with low; switch the zero padding with 1s where low has 1s.
+    Low will only compare to the zero padding since it has 8 bits
+    while high now has 16
+    */
+    const opcode = (high << 8) | low;
+    //Jump to next instruction
+    this.programCounter += 2;
   }
   call(address) {
     this.stack[stackPointer] = this.programCounter;
--- main.js
+++ main.js
@@ -8,3 +8,24 @@ const font = new Font(ram.data);
 font.load();
 const display = new Display();
 const cpu = new Cpu(ram.data, display)
+
+const playBeep = () => {
+  process.stdout.write('\x07'); //terminal beep
+}
+
+const cpuLoop = () => {
+  cpu.step();
+  setImmediate(cpuLoop);
+}
+
+const timerLoop = () => {
+  if (cpu.delayTimer > 0) cpu.delayTimer--;
+  if (cpu.soundTimer > 0) {
+    cpu.soundTimer--;
+    playBeep();
+  }
+  setTimeout(timerLoop, 1000 / 60);
+}
+
+cpuLoop();
+timerLoop();
