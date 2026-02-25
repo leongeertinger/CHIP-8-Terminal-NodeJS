f129893657a21d5e68ea872799f05d3035b056b3
Leon <leon_geertinger@hotmail.com>
2026-02-25 09:21:49 +0100

    Add decode of opcode and minor bug fixes

--- cpu.js
+++ cpu.js
@@ -1,4 +1,5 @@
 export class Cpu {
+
   constructor(memory, display, keypad) {
     this.memory = memory;
     this.display = display;
@@ -27,13 +28,68 @@ export class Cpu {
     const opcode = (high << 8) | low;
     //Jump to next instruction
     this.programCounter += 2;
+    this.decode(opcode);
+  }
+  decode(opcode) {
+    const nnn = opcode & 0x0FFF; //12 bit adress
+    const kk = opcode & 0x00FF; //8 bit
+    const n = opcode & 0x000F; //4 bit
+    const x = (opcode & 0x0F00) >> 8; //X register index
+    const y = (opcode & 0x00F0) >> 4; //Y register index
+    switch (opcode & 0xF000) {
+      case 0x0000:
+        if (opcode === 0x00E0) {
+          //00E0: clear screen
+          this.display.clear();
+          break;
+        }
+        else if (opcode === 0x00EE) {
+          //00EE: return from subroutine
+          this.ret();
+          break;
+        }
+        else {
+          console.warn(`Unknown opcode 0x${opcode.toString()}`);
+        }
+      case 0x1000:
+        //1NNN: Jump to address NNN
+        this.programCounter = nnn;
+        break;
+      case 0x2000:
+        //2NNN: Call address
+        this.call(nnn);
+        break;
+      case 0x6000:
+        //6XNN: Set regsiter Vx
+        this.V[x] = kk;
+        break;
+      case 0x7000:
+        //7XNN: Add KK to Vx
+        this.V[x] += (this.V[x] + kk) & 0xFF;
+        break;
+      case 0xA000:
+        //ANNN: set index I
+        this.I = nnn;
+        break;
+      case 0xD000:
+        //DXYN: draw/display
+        break;
+      default:
+        console.warn(`Unknown opcode 0x${opcode.toString()}`);
+    }
   }
   call(address) {
-    this.stack[stackPointer] = this.programCounter;
+    if (this.stackPointer >= this.stack.length) {
+      throw new Error('Stack overflow');
+    }
+    this.stack[this.stackPointer] = this.programCounter;
     this.stackPointer++;
     this.programCounter = address;
   }
-  return() {
+  ret() {
+    if (this.stackPointer === 0) {
+      throw new Error('Stack underflow')
+    }
     this.stackPointer--;
     this.programCounter = this.stack[this.stackPointer];
   }
